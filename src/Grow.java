460767d3399bbd69e9b515161236799035b9f08a
dclarke3-github <dclarke3@corning-cc.edu>
2022-10-25 14:53:32 -0400

    Newer design for growth. Will need to implement LCG and a weather class.

---
+++ src/Grow.java
@@ -0,0 +1,19 @@
+class Grow {
+    int nutrientLVL;
+    int waterLVL;
+    int sunLVL;
+
+    IncrementGrowth(){
+            nutrientLVL  =0
+            waterLVL     =0
+            sunLVL       =0
+
+
+    }
+    Cycle(){
+        nutrientLVL=/* random number high low | odds will produce fruits and better yields. while evens will give more
+        symmetric plants.
+        waterLVL=/* weather chosen from the weather class with values for how much water it gives.
+        sunLVL=/* weather chosen from the weather class with values for how much sun it gives.
+    }
+}
--- src/decay.java
+++
@@ -1,4 +0,0 @@
-class decay {
-    int rate;
- /* Still looking at how I want to implement this math operation
-}
